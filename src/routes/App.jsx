0fcdeca706cbdac5c7671e5483b84bc8dbba1ff2
Ing. Nelson Gonzalez <nescalan@gmail.com>
2021-01-22 10:37:50 -0600

    Routes: first commit App.js

---
+++ src/routes/App.jsx
@@ -0,0 +1,19 @@
+// import Libraries
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
+
+// import Components
+import Home from '../containers/Home'
+
+const App = () =>  {
+    state = {  }
+    render() { 
+        return ( 
+            <BrowserRouter>
+                <Route path="/" component={Home} />
+            </BrowserRouter>
+         );
+    }
+}
+ 
+export default App;
