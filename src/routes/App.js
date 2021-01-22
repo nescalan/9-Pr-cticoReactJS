2e017c3d88450eec3832b02326375939451523bb
Ing. Nelson Gonzalez <nescalan@gmail.com>
2021-01-22 13:47:13 -0600

    3: Routs File: BrowseRouter and Route

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,16 +1,14 @@
 // import Libraries
+import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
 // import Components
 import Home from "../containers/Home";
 
-const App = () => {
-  render();
-  return (
-    <BrowserRouter>
-      <Route exact path="/" component={Home} />
-    </BrowserRouter>
-  );
-};
+const App = () => (
+  <BrowserRouter>
+    <Route exact path="/" component={Home} />
+  </BrowserRouter>
+);
 
 export default App;
