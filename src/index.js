5e07d9a45f8e4b228560b43df78e3f1d002747ac
Ing. Nelson Gonzalez <nescalan@gmail.com>
2021-01-25 16:48:50 -0600

    index.js: import Provider and createStore

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+// import Libraries
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+// import Components
 import App from "./routes/App";
 
 ReactDOM.render(<App />, document.getElementById("app"));
