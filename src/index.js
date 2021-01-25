03a3f6b37c2cb325f00b5719eaf93579b8ec0ab0
Ing. Nelson Gonzalez <nescalan@gmail.com>
2021-01-25 16:51:31 -0600

    First Provider on index.js src file

--- src/index.js
+++ src/index.js
@@ -7,4 +7,9 @@ import { createStore } from "redux";
 // import Components
 import App from "./routes/App";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
