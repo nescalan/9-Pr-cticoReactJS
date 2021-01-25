a703fd4abf0b29bd6a09973e8e5ce2f1efc7736c
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2021-01-24 18:19:29 -0600

    6: NotFound.scss Init

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,14 @@
+// import Libraries
+import React from "react";
+
+// import Styles and Images
+
+const NotFound = () => {
+  return (
+    <React.Fragment>
+      <h1>I'm Error 404</h1>
+    </React.Fragment>
+  );
+};
+
+export default NotFound;
