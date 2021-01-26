fc74c879c46114b49c58287ccd12713a09db9ed5
Ing. Nelson Gonzalez <nescalan@gmail.com>
2021-01-26 16:46:25 -0600

    Home.jsx: Messages Updated

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,11 +1,16 @@
+// import Libraries
 import React, { useState, useEffect } from "react";
+import useInitialState from "../hooks/useInitialState";
+import { connect } from "react-redux";
+
+// import Styles and Images
+import "../assets/styles/App.scss";
+
+// import Components
 import Search from "../components/Search";
 import Categories from "../components/Categories";
 import Carousel from "../components/Carousel";
 import CarouselItem from "../components/CarouselItem";
-import Footer from "../components/Footer";
-import useInitialState from "../hooks/useInitialState";
-import "../assets/styles/App.scss";
 
 const API = "http://localhost:3000/initalState";
 
